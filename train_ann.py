dab3a4a17a7de9a58d8db20466706717b2409350
Yogesh Gajare <yogeshygajare@gmail.com>
2025-06-09 12:18:39 +0530

    Update train_ann.py

--- train_ann.py
+++ train_ann.py
@@ -118,7 +118,9 @@ X_test = scaler.transform(X_test)
 
 # Build ANN model
 model = keras.Sequential([
-    keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    #keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    keras.Input(shape=(X_train.shape[1],)),
+    keras.layers.Dense(32, activation='relu'),
     keras.layers.Dense(16, activation='relu'),
     keras.layers.Dense(1, activation='sigmoid')  # Binary classification
 ])
